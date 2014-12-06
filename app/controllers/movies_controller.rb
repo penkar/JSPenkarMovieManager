66ade2cd7be99f976d5f61deb3656bcbb57838e1
jsp <jspenkar@gmail.com>
2014-12-05 19:58:31 -0600

    Movies index is up. Controller events have been created.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,23 @@
 class MoviesController < ApplicationController
+	def index
+		@movies = Movie.all
+	end
+
+	def create
+	end
+
+	def new
+	end
+
+	def show
+	end
+
+	def update
+	end
+
+	def update
+	end	
+
+	def destroy
+	end	
 end
